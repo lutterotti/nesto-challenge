5420dae630ab04066bbc1a6761b0c69c2deee446
lutterotti <11467360+lutterotti@users.noreply.github.com>
2021-05-21 05:09:01 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './features/styles/MainStyles.scss';
+import UserLogin from './features/login/UserLogin';
+import { useAppSelector } from './store/hooks';
+import { userIsLoggedIn } from './store/userSlice';
+import MainPage from './features/main-page/MainPage';
+
+function displayCurrentPage(user_is_logged_in: boolean) {
+  if (user_is_logged_in) {
+    return (<MainPage></MainPage>);
+  } else {
+    return (<UserLogin></UserLogin>);
+  }
+}
+
+const App: React.FC<{}> = () => {
+  const user_is_logged_in: boolean = useAppSelector(userIsLoggedIn);
+  return (
+    <div className="platform">
+      {displayCurrentPage(user_is_logged_in)}
+    </div>
+  )
+}
+
+export default App;
